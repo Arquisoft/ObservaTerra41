2c223fcfdae72fbfe09d4c2db95c8be3dab03cfc
DanteInferno <sendasoscuras@hotmail.com>
2014-05-04 16:06:24 +0200

    Añadidos pequeños test a JsonParser

--- app/utils/CSVReader.java
+++ app/utils/CSVReader.java
@@ -90,26 +90,6 @@ public class CSVReader {
 			
 		}
 
-//		String texto = responseStrBuilder.toString();
-//		String[] cadenas = texto.split("--");
-//		int i = 1;
-//		while (i < cadenas.length) {
-//
-//			String[] filas = cadenas[i].split(";");
-//			for (int k = 0; k < filas.length; k += 3) {
-//				System.out.println(filas[k] + filas[k + 1] + filas[k + 2]);
-//				String country = filas[k];
-//				String indicator = filas[k + 1];
-//				String valueString = filas[k + 2];
-//				Double value = Double.valueOf(valueString);
-//				Observation observation = new Observation(country, indicator,
-//						value);
-//				obsList.add(observation);
-//				System.out.println(observation);
-//			}
-//			i++;
-//
-//		}
 
 		return obsList;
 	}
