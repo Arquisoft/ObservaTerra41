97d2ffdea690304a03ed90b0b54dc4821637b746
datego <datego92@hotmail.com>
2014-05-04 12:32:25 +0200

    Subiendo de nuevo el cliente en c# (corrigiendo errores con git)

---
+++ ClienteAPI_C#/PruebaAPI/PruebaAPI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyWebRequest
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create the constructor with post type and few data
+            MyWebClient myRequest = new MyWebClient();
+            //show the response string on the console screen.
+            Console.WriteLine(myRequest.takeResources());
+        }
+    }
+}
